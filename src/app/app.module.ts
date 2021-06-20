6a6aad63c78fcedeeebdf5db535f4e3a4364c838
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-20 19:20:10 +0200

    creation of app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Error404Component } from './shared/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
