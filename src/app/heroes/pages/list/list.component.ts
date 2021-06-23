69a43d202cb6098178b50c43fbf65f1a172454c7
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-23 09:53:46 +0200

    implements mat-card directives

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -6,6 +6,11 @@ import {Heroe} from "../../interfaces/heroes.interface";
   selector: 'app-list',
   templateUrl: './list.component.html',
   styles: [
+    `
+    mat-card{
+      margin-top: 20px;
+    }
+    `
   ]
 })
 export class ListComponent implements OnInit {
