fc5b7e20e51f6227a816d3fbbf7f84a44a4498d3
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-23 10:48:29 +0200

    create heroe-card.component to refactor app

---
+++ src/app/heroes/components/heroe-card/heroe-card.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Heroe} from "../../interfaces/heroes.interface";
+
+@Component({
+  selector: 'app-heroe-card',
+  templateUrl: './heroe-card.component.html',
+  styles: [
+    `
+    mat-card{
+      margin-top: 20px;
+    }
+    `
+  ]
+})
+export class HeroeCardComponent implements OnInit {
+
+  @Input() heroe!: Heroe;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
