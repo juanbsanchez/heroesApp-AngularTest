5f71f490b59775d9c1c6fd287bcfc2dedeb631e9
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-24 19:08:00 +0200

    implement method optionSelected to show suggestions, options, or info string to user

--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -12,22 +12,29 @@ import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 export class SearchComponent implements OnInit {
   term: string = '';
   heroes: Heroe[] = [];
-  heroeSelected!: Heroe;
+  heroeSelected!: Heroe | undefined;
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
   searching(){
-    this.heroesService.getSuggestions(this.term)
+    this.heroesService.getSuggestions(this.term.trim())
       .subscribe(heroes => this.heroes = heroes);
   }
 
   optionSelected(event: MatAutocompleteSelectedEvent){
-    const heroe: Heroe = event.option.value;
-    this.term = heroe.superhero;
 
-    this.heroesService.getHeroebyId(heroe.id!)
-      .subscribe(heroe => this.heroeSelected = heroe)
+    if(event.option.value !== ''){
+      const heroe: Heroe = event.option.value;
+      this.term = heroe.superhero;
+
+      this.heroesService.getHeroebyId(heroe.id!)
+        .subscribe(heroe => this.heroeSelected = heroe)
+    }
+
+    this.heroeSelected = undefined;
+    return;
+
   }
 }
