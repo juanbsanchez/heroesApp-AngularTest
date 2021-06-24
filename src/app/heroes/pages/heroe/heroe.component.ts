9a517b496e2e7bc2ed20c29f6f010f52263d3161
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-24 11:24:28 +0200

    layout of heroe page

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Heroe} from "../../interfaces/heroes.interface";
 import {HeroesService} from "../../services/heroes.service";
 import {switchMap} from "rxjs/operators";
@@ -9,6 +9,12 @@ import {switchMap} from "rxjs/operators";
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: [
+    `
+    img{
+      width: 70%;
+      border-radius: 5px;
+    }
+    `
   ]
 })
 export class HeroeComponent implements OnInit {
@@ -18,7 +24,8 @@ export class HeroeComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -28,7 +35,10 @@ export class HeroeComponent implements OnInit {
        switchMap(({id}) => this.heroesService.getHeroebyId(id))
      ).subscribe(heroe => this.heroe = heroe)
 
-    
+  }
+
+  back(){
+    this.router.navigate(['/heroes/list'])
   }
 
 }
