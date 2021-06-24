cc8fda7cb4514c584c3bfce7db70a438a345dc08
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-24 18:37:22 +0200

    call method to get suggestions and implements on searcher

--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Heroe} from "../../interfaces/heroes.interface";
 import {HeroesService} from "../../services/heroes.service";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'app-search',
@@ -11,13 +12,22 @@ import {HeroesService} from "../../services/heroes.service";
 export class SearchComponent implements OnInit {
   term: string = '';
   heroes: Heroe[] = [];
+  heroeSelected!: Heroe;
   constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
   searching(){
-    this.heroesService.getHeroes()
+    this.heroesService.getSuggestions(this.term)
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  optionSelected(event: MatAutocompleteSelectedEvent){
+    const heroe: Heroe = event.option.value;
+    this.term = heroe.superhero;
+
+    this.heroesService.getHeroebyId(heroe.id!)
+      .subscribe(heroe => this.heroeSelected = heroe)
   }
 }
