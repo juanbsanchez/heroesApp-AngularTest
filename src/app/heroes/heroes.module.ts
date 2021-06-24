b372df9e8f956d1b340625ae646b1b9048c18499
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-24 12:21:24 +0200

    implements autocomplete component on search page

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import {FlexLayoutModule} from "@angular/flex-layout";
 import {MaterialModule} from "../material/material.module";
 import { HeroeCardComponent } from './components/heroe-card/heroe-card.component';
 import { ImagenPipe } from './pipes/imagen.pipe';
+import {FormsModule} from "@angular/forms";
 
 
 
@@ -27,7 +28,8 @@ import { ImagenPipe } from './pipes/imagen.pipe';
     CommonModule,
     HeroesRoutingModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule
   ]
 })
 export class HeroesModule { }
--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Heroe} from "../../interfaces/heroes.interface";
+import {HeroesService} from "../../services/heroes.service";
 
 @Component({
   selector: 'app-search',
@@ -7,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  term: string = '';
+  heroes: Heroe[] = [];
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
   }
 
+  searching(){
+    this.heroesService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
